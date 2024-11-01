6893387c11a1e2c40250333a30bf08e968df53b9
humbertocortezia <humbertocortezia@seduc.ro.gov.br>
2024-11-01 09:19:11 -0300

    ajustando rotas

--- config/routes.rb
+++ config/routes.rb
@@ -1,25 +1,8 @@
 Rails.application.routes.draw do
-  devise_for :users, skip: [:sessions, :registrations] # Desabilita as rotas padrão para sessões e registros
-
-  # Rotas de Sessões
-  devise_scope :user do
-    get    'users/sign_in', to: 'devise/sessions#new', as: :new_user_session
-    post   'users/sign_in', to: 'devise/sessions#create', as: :user_session
-    delete 'logout', to: 'devise/sessions#destroy', as: :destroy_user_session # Novo caminho para logout
-  end
-
-  # Rotas de Registro
-  devise_scope :user do
-    get    'users/sign_up', to: 'devise/registrations#new', as: :new_user_registration
-    post   'users', to: 'devise/registrations#create', as: :user_registration
-    get    'users/edit', to: 'devise/registrations#edit', as: :edit_user_registration
-    patch  'users', to: 'devise/registrations#update'
-    put    'users', to: 'devise/registrations#update'
-    delete 'users', to: 'devise/registrations#destroy'
-  end
+  devise_for :users, only: [:sessions] # Desabilita as rotas padrão para sessões e registros
 
   # Outras rotas
-  resources :users, as: 'usuarios' 
+  resources :users, as: 'usuarios'
   resources :agendamentos
   resources :disponibilidade_consultors
   resources :cidades
